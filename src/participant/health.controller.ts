f4363ee669786ee3d43959486f2212df3f534587
aaron <charlestrop12@gmail.com>
2025-09-12 20:37:28 +0000

    mise a jout de secure

--- src/participant/health.controller.ts
+++ src/participant/health.controller.ts
@@ -1,11 +1,18 @@
 // health.controller.ts
 import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PrismaService } from 'prisma/prisma.service';
 
-@Controller()
+@Controller('v1/health')
 export class HealthController {
   constructor(private prisma: PrismaService) {}
+@ApiTags('APIs health')
 @Get('/health')
+@ApiResponse({
+  status: 200,
+  description: 'Vérifie la santé de l\'application et la connexion à la base de données.',
+})
+@ApiOperation({ summary: 'Vérifie la santé de l\'application et la connexion à la base de données.' })
 async healthCheck() {
   try {
     // Vérifiez la connexion à la DB avec Prisma
